3dae3eb48f7d7f3616f4c1f2df42741ee6ed787c
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-05-06 16:11:40 -0300

    first commit

---
+++ animacao.js
@@ -0,0 +1,26 @@
+window.animacao = ScrollReveal({ reset: true });
+
+animacao.reveal('.navbar', {
+    duration: 100,
+    distance: '30px',
+})
+
+animacao.reveal('.content-container', {
+    duration: 500,
+    distance: '30px',
+})
+
+animacao.reveal('.fs-4', {
+    duration: 500,
+    distance: '30px',
+})
+
+animacao.reveal('.fs-1', {
+    duration: 500,
+    distance: '30px',
+})
+
+animacao.reveal('.image-formula', {
+    duration: 500,
+    distance: '30px',
+})
