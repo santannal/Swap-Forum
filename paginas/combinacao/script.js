80029b1073fa13e890e097c8947e0c97c51ffea8
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-05-14 17:21:27 -0300

    ajustes finais

--- paginas/combinacao/script.js
+++ paginas/combinacao/script.js
@@ -6,43 +6,13 @@ function calcularPermutacao(numero) {
     return fatorial;
 }
 
-function calcularFatorial(numero) {
-    if (numero === 0 || numero === 1) {
-        return 1;
-    } else {
-        return numero * calcularFatorial(numero - 1);
-    }
-}
-
-function calcularPermutacaoComRepeticao(entrada) {
-    var entradaArray = entrada.split('');
-
-    var contagemCaracteres = {};
-    entradaArray.forEach(function (caractere) {
-        if (contagemCaracteres[caractere]) {
-            contagemCaracteres[caractere]++;
-        } else {
-            contagemCaracteres[caractere] = 1;
-        }
-    });
-
-    var fatorialEntrada = calcularFatorial(entradaArray.length);
-
-    var fatoriaisContagem = 1;
-    for (var caractere in contagemCaracteres) {
-        fatoriaisContagem *= calcularFatorial(contagemCaracteres[caractere]);
-    }
-
-    return fatorialEntrada / fatoriaisContagem;
-}
-
 document.getElementById('calcular').addEventListener('click', function () {
-    var entradan = document.getElementById('input').value;
-    var entradak = document.getElementById('inputvaluek').value;
-    var isNumero = /^\d+$/.test(entradan);
+    var entradan = parseInt(document.getElementById('input').value);
+    var entradak = parseInt(document.getElementById('inputvaluek').value);
+    var isNumero = /^\d+$/.test(entradan) && /^\d+$/.test(entradak);
 
     if (isNumero) {
-        if (document.getElementById('input') >= document.getElementById('inputvaluek')) {
+        if (entradan >= entradak) {
             if (document.getElementById("radiocombinacao").checked) {
                 var resultado = calcularPermutacao(entradan) / (calcularPermutacao(entradak) * (calcularPermutacao(entradan - entradak)));
                 document.getElementById('resultado').innerHTML = 'Combinação simples: ' + resultado.toFixed(0);
@@ -51,31 +21,9 @@ document.getElementById('calcular').addEventListener('click', function () {
                 document.getElementById('resultado').innerHTML = 'Arranjo simples: ' + resultado.toFixed(0);
             }
         } else {
-            document.getElementById('resultado').innerHTML = 'O k precisa ser menor ou igual n';
+            document.getElementById('resultado').innerHTML = 'O VALOR DE K PRECISA SER MENOR OU IGUAL AO VALOR DE N';
         }
     } else {
         document.getElementById('resultado').innerHTML = 'Utilize a calculadora de permutação';
     }
 });
-
-/*
-document.getElementById('btnmore').addEventListener('click', function () {
-    const elementoPai = document.getElementById('novoitem');
-
-    const novoInput = document.createElement("input");
-    novoInput.className = 'form-control form-control-lg mb-2';
-    novoInput.placeholder = "Insira a repetição...";
-    novoInput.type = "number";
-    novoInput.id = "novoInput";
-
-    elementoPai.appendChild(novoInput);
-})
-
-document.getElementById('btnmenos').addEventListener('click', function () {
-
-    const elementoPai = document.getElementById('novoitem');
-    const inputs = elementoPai.querySelectorAll('input');
-    const ultimoInput = inputs[inputs.length - 1];
-    elementoPai.removeChild(ultimoInput);
-})
-*/
