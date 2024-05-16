39a7b514296a6ae2801e4e09dec9af3155daaddf
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-05-16 19:15:18 -0300

    coments

--- paginas/combinacao/script.js
+++ paginas/combinacao/script.js
@@ -1,3 +1,4 @@
+//função para que se realize o cálculo dos elementos
 function calcularPermutacao(numero) {
     var fatorial = 1;
     for (var i = 1; i <= numero; i++) {
@@ -6,13 +7,19 @@ function calcularPermutacao(numero) {
     return fatorial;
 }
 
+//evento relacionado ao botão "calcular"
 document.getElementById('calcular').addEventListener('click', function () {
+    //pega os valores dos inputs (n e k), transformando-os em inteiros
     var entradan = parseInt(document.getElementById('input').value);
     var entradak = parseInt(document.getElementById('inputvaluek').value);
+
+    //verifica se ambos são numeros
     var isNumero = /^\d+$/.test(entradan) && /^\d+$/.test(entradak);
 
     if (isNumero) {
+        //verifica se n é maior ou igual k
         if (entradan >= entradak) {
+            //verifica qual botão está "setado", no qual define o tipo de operação
             if (document.getElementById("radiocombinacao").checked) {
                 var resultado = calcularPermutacao(entradan) / (calcularPermutacao(entradak) * (calcularPermutacao(entradan - entradak)));
                 document.getElementById('resultado').innerHTML = 'Combinação simples: ' + resultado.toFixed(0);
